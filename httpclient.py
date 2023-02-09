22639e24a6f60e18f742540755baaabce0fae8cf
libbusUA <libbus@ualberta.ca>
2023-02-09 15:06:26 -0700

    Completed assignment2. All tests pass in the freetests.py file.

--- httpclient.py
+++ httpclient.py
@@ -24,8 +24,6 @@ import re
 # you may use urllib to encode data appropriately
 import urllib.parse
 
-from parso import parse
-from zmq import PROTOCOL_ERROR_ZMTP_MALFORMED_COMMAND_UNSPECIFIED
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -44,13 +42,21 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        headers = self.get_headers(data)
+        status_line = headers[0].split(" ")
+        code = int(status_line[1])
+        return code
 
     def get_headers(self,data):
-        return None
+        response = data.split("\r\n\r\n")
+        headers = response[0].split("\r\n")
+        
+        return headers
 
     def get_body(self, data):
-        return None
+        response = data.split("\r\n\r\n")
+        body = response[1]
+        return body
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -76,19 +82,53 @@ class HTTPClient(object):
         
         parsedURL = urllib.parse.urlparse(url)
         host = parsedURL.hostname
-        path = parsedURL.path
-        port = parsedURL.port
-        request = "GET "+  path +  " HTTP/1.1\r\nHost:" + host + "\n\n"
         
+        path = parsedURL.path
+        if path == '':
+            path = '/'
+        port = parsedURL.port
+        userAgent = "almostcURL/1.0"
+
+        if args != None:
+            arguments = ""
+            i = 0
+            for key in args:
+                
+                if i > 0:
+
+                    arguments = arguments + "&" + key + "=" + args[key]
+                else:
+                    arguments = arguments + key + "=" + args[key]
+                i +=1
+            request = "GET "+  path + '?' + arguments + " HTTP/1.1\r\n"
+        elif parsedURL.query != '':
+            path += '?' + parsedURL.query
+            request = "GET "+  path + " HTTP/1.1\r\n"
+        else:
+            request = "GET "+  path +  " HTTP/1.1\r\n" 
+
+        #Maybe deal with fragments? 
+
+        request += "Host: " + host + "\r\n"
+        request += "User-Agent: " + userAgent + "\r\n"
+        request += "Accept-Encoding: utf-8\r\n"
+        request += "Connection: close\r\n\r\n"
+
+        
+
+
         if port != None:
             self.connect(host, port)
         else:
             self.connect(host, 80)
         
+
         self.sendall(request)
         
-        body = self.recvall(self.socket)
-        code = int(body.split()[1])
+        
+        data = self.recvall(self.socket)
+        body = self.get_body(data) 
+        code = self.get_code(data)
 
         self.close()
 
@@ -109,20 +149,31 @@ class HTTPClient(object):
         request += path + " HTTP/1.1\r\n"
         request += "HOST: " +  host + " \r\n"
         request += "User-Agent: " +  userAgent + " \r\n"
-        request += "Content-Type: " +  content_type + " \r\n\r\n"
+        request += "Connection: close\r\n"
+        request += "Content-Type: " +  content_type + " \r\n"
         # send a content length in here. 
+        
         if args != None:
+            arguments = ""
             i = 0
             for key in args:
                 
                 if i > 0:
 
-                    request = request + "&" + key + "=" + args[key]
+                    arguments = arguments + "&" + key + "=" + args[key]
                 else:
-                    request = request + key + "=" + args[key]
+                    arguments = arguments + key + "=" + args[key]
                 i +=1
 
+            content_length = len(arguments)
+            request += "Content-Length: " + str(content_length) + "\r\n\r\n"
+            request += arguments
+        else:
+            request += "Content-Length: 0\r\n\r\n"
 
+        
+
+        
 
         if port != None:
             self.connect(host, port)
@@ -130,8 +181,10 @@ class HTTPClient(object):
             self.connect(host, 80)
         
         self.sendall(request)
-        body = self.recvall(self.socket)
-        code = int(body.split()[1])
+        data = self.recvall(self.socket)
+        code = self.get_code(data)
+        body = self.get_body(data)
+
         self.close()
 
         #You are not completing the body of your request. Something is wrong here and HTTP is waiting too long.
